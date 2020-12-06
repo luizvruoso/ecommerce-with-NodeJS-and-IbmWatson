4fe033b3f874dafffca96835f684dfafd5e71f3b
Luiz Vinícius Ruoso <53343378+luizvruoso@users.noreply.github.com>
2020-12-06 15:24:57 -0300

    Update routes.js

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -6,27 +6,27 @@ module.exports = function (route) {
     // GET na raiz logada (AKA Dashboard)
     
     route.get('/', function (req, res) {
-        route.app.controller.controllerStore.index(route, req, res);
+        route.app.app.controller.controllerStore.index(route, req, res);
         //route acessa o app, o app o controller que enfim acessa chatbot que renderiza index;
     });
     route.get('/index', function (req, res) {
-        route.app.controller.controllerStore.index(route, req, res);
+        route.app.app.controller.controllerStore.index(route, req, res);
         //route acessa o app, o app o controller que enfim acessa chatbot que renderiza index;
     });
     route.get('/carrinho', function (req, res) {
-        route.app.controller.controllerStore.carrinho(route, req, res);
+        route.app.app.controller.controllerStore.carrinho(route, req, res);
         //route acessa o app, o app o controller que enfim acessa chatbot que renderiza index;
     });
     route.get('/produto', function (req, res) {
-        route.app.controller.controllerStore.produto(route, req, res);
+        route.app.app.controller.controllerStore.produto(route, req, res);
     });
     
     route.get('/chatbot', function (req, res) {
-        route.app.controller.controllerStore.chatbot(route, req, res);
+        route.app.app.controller.controllerStore.chatbot(route, req, res);
         //route acessa o app, o app o controller que enfim acessa chatbot que renderiza index;
     });
     route.post('/assistant', function (req, res) {
-        route.app.controller.controllerStore.callAssistant(route, req, res);
+        route.app.app.controller.controllerStore.callAssistant(route, req, res);
     });
    
 }
